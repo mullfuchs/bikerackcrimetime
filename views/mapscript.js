9fe063bb4311856f39b8937b09d986b3111ab453
mullfuchs <burke.leo@gmail.com>
2016-12-09 13:33:49 -0800

    adding city bike rack data to application

--- views/mapscript.js
+++ views/mapscript.js
@@ -1,6 +1,9 @@
 //mapscript.js
 
 var map;
+
+var bikeRacks;
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
@@ -34,3 +37,41 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
 }
+
+//https://data.seattle.gov/resource/fxh3-tqdm.json
+$.ajax({
+    url: "https://data.seattle.gov/resource/fxh3-tqdm.json",
+    type: "GET",
+    data: {
+      "$limit" : 100,
+      "$$app_token" : "97k4LhfmpIDqhsNhi00jZAyl3"
+    }
+}).done(function (data) {
+  // body...
+  //alert("Retrieved " + data.length + " records from the dataset!");
+  //console.log(data);
+  //bikeRacks = data;
+
+  for(var i = 0; i < data.length; i++){
+    //console.log(data[i].unitid);
+     console.log(parseFloat( data[i].latitude ));
+     console.log(parseFloat( data[i].longitude));
+    var theLatLng = {lat: parseFloat( data[i].latitude ), lng: parseFloat( data[i].longitude )};
+
+    var marker = new google.maps.Marker({
+      position: theLatLng,
+      title: data[i].unitid
+    });
+
+    marker.setMap(map);
+  }
+
+  // var marker = new google.maps.Marker({
+  //   position: myLatLng,
+  //   map: map,
+  //   title: 'Hello World!'
+  // });
+
+});
+
+
